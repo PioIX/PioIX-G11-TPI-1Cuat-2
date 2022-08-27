0218c893a3f5926fa2a4d7b131717c725dd6eb61
Damian Efrain <101183244+dbottini2022pioix@users.noreply.github.com>
2022-08-27 03:07:32 +0000

    all

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import request
+import json
 
-app = Flask(__name__)
+app = Flask('app')
+
 
 @app.route('/')
 def index():
-    return 'Hello, from Flask!'
+    return render_template('index.html')
 
-app.run(host='0.0.0.0', port=81)
+
+@app.route('/cody')
+def cody():
+    return render_template('cody.html')
+
+
+@app.route('/memo')
+def memo():
+    return render_template('memo.html')
+
+@app.route('/ganarMemo')
+def ganarMemo():
+  return render_template('memoGanar.html')
+
+@app.route('/test', methods=['POST'])
+def test():
+  output = request.get_json()
+  print(output)
+  result = json.loads(output)
+  print(result)
+  return result
+
+
+# usar Flask session para guardar datos según el usuario
+  
+app.run(host='0.0.0.0', port=8080)
